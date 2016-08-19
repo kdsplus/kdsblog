66ff84b6271862a0e1531ba9ec581895d8cd392e
kdsplus <kdsplus@gmail.com>
2016-08-20 05:02:22 +0900

    add

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['E:/project/python_test/mysite/templates',],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
