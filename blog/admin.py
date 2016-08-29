fd85771716223db3db2da600094797f9468fc2ca
kdsplus <kdsplus@gmail.com>
2016-08-29 16:32:52 +0900

    add job

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,11 @@
 #-*- coding: utf-8 -*-
 
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
 # 관리자 페이지에서 만든 모델을 보려면
 # cf https://docs.djangoproject.com/en/1.8/ref/contrib/admin/ 관리자에 대해서~
 admin.site.register(Post)
+admin.site.register(Comment)
